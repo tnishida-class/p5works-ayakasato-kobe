9e01d6a901b7ca0868a9add521571ecfdfd37e9c
ayakasato-kobe <ayakasato.kobe@gmail.com>
2020-11-17 16:30:25 +0900

    Update-section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -7,11 +7,18 @@ function setup(){
   y = height / 2;
   vx = 8;
   vy = 8;
+  angle=0;
 }
 
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
+
+  star(x,y,20,angle);
+  x -= 1.4;
+  y += 1.4;
+  angle += 0.04;
+
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -21,13 +28,15 @@ function draw(){
 }
 
 function star(cx, cy, r, angle){
+  noStroke();
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI +angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
+
   }
   endShape(CLOSE);
 }
