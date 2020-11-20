235b00fc768093342a3483edf558df2faf2bd3f3
ayakasato-kobe <ayakasato.kobe@gmail.com>
2020-11-20 15:48:45 +0900

    final1

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,18 @@
 // 最終課題を制作しよう
+//https://tnishida-class.github.io/p5works-ayakasato-kobe/finalwork/index.html
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 660);
+}
+
+let img;
+function preload(){
+  img = loadImage('test.png');
 }
 
 function draw(){
   background(160, 192, 255);
-}
+  image(img,0,0);
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+
 }
